80e5f57fdafadec02c9fa28c3df98eb298da9d78
EteWin <sakaguchi.takumi03@gmail.com>
2026-03-28 15:45:50 +0900

    CSSを適用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 export default function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [openKey, setOpenKey] = useState<number | null>(null);
 
   type Todo = {
     readonly key: number;
     title: string;
     completed: boolean;
     removed: boolean;
+    memo?: string;
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,7 +31,7 @@ export default function App() {
 
   const patchTodo = (
     key: number,
-    patches: Partial<Pick<Todo, "title" | "completed" | "removed">>,
+    patches: Partial<Pick<Todo, "title" | "completed" | "removed" | "memo">>,
   ) => {
     setTodos((todos) =>
       todos.map((todo) => {
@@ -42,39 +44,87 @@ export default function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
         }}
       >
-        <input type="text" value={text} onChange={(e) => handleChange(e)} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => handleChange(e)}
+          placeholder="タスクを入力"
+        />
         <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
       <ul>
         {todos.map((todo) => {
           return (
-            <li key={todo.key}>
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                disabled={todo.removed}
-                onChange={() =>
-                  patchTodo(todo.key, { completed: !todo.completed })
-                }
-              ></input>
-              <input
-                type="text"
-                disabled={todo.completed || todo.removed}
-                value={todo.title}
-                onChange={(e) => patchTodo(todo.key, { title: e.target.value })}
-              />
-              <button
-                onClick={() => patchTodo(todo.key, { removed: !todo.removed })}
-              >
-                {todo.removed ? "復元" : "削除"}
-              </button>
+            <li className="todo-item" key={todo.key}>
+              <div className="todo-row">
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    disabled={todo.removed}
+                    onChange={() =>
+                      patchTodo(todo.key, { completed: !todo.completed })
+                    }
+                  />
+                  <span className="slider"></span>
+                </label>
+                <input
+                  type="text"
+                  disabled={todo.completed || todo.removed}
+                  value={
+                    todo.removed
+                      ? "(削除済み)"
+                      : todo.completed
+                        ? `${todo.title}（完了）`
+                        : todo.title
+                  }
+                  maxLength={16}
+                  onChange={(e) =>
+                    patchTodo(todo.key, { title: e.target.value })
+                  }
+                  placeholder="タスクを編集"
+                />
+                <button
+                  className={`memo-toggle ${openKey === todo.key ? "open" : ""}`}
+                  disabled={todo.removed}
+                  onClick={() =>
+                    setOpenKey(openKey === todo.key ? null : todo.key)
+                  }
+                >
+                  ◀
+                </button>
+
+                <button
+                  onClick={() => {
+                    const isRemoved = !todo.removed;
+                    patchTodo(todo.key, { removed: isRemoved });
+                    if (isRemoved && openKey == todo.key) {
+                      setOpenKey(null);
+                    }
+                  }}
+                >
+                  {todo.removed ? "復元" : "削除"}
+                </button>
+              </div>
+              {openKey === todo.key && (
+                <div className="todo-memo-detail">
+                  <textarea
+                    disabled={todo.removed}
+                    value={todo.memo || ""}
+                    placeholder="メモを入力..."
+                    onChange={(e) =>
+                      patchTodo(todo.key, { memo: e.target.value })
+                    }
+                  />
+                </div>
+              )}
             </li>
           );
         })}
